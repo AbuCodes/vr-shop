8ec72f0c0b5daa15c0b8d065ea2567486040e593
AbuCodes <abuadel251@gmail.com>
2020-12-26 00:25:57 -0700

    created product controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,7 +1,6 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
 const router = express.Router();
-import Product from '../models/productModel.js';
+import { getProducts, getProductById } from '../controllers/productController.js';
 
 // import {
 //   getProducts,
@@ -14,26 +13,8 @@ import Product from '../models/productModel.js';
 // } from '../controllers/productController.js';
 // import { protect, admin } from '../middleware/authMiddleware.js';
 
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Product not found');
-    }
-  })
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 // router.route('/').get(getProducts).post(protect, admin, createProduct);
 // router.route('/:id/reviews').post(protect, createProductReview);
